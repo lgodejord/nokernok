b4c32dcc86724a6a4f43695678a7914a2c0c883b
lgodejord <lars.godejord@makingwaves.no>
2013-10-08 13:21:29 +0200

    first checkin

---
+++ nokernok/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace nokernok
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
